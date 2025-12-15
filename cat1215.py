b7f9eda434e12b52197544076e9b8a702f68a877
zoraluo <449290265@qq.com>
2025-12-15 16:54:38 +0800

    Create cat1215.py
    新建了一个cat文件

---
+++ cat1215.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+# ASCII 小猫
+cat = r"""
+ /\_/\ 
+( o.o )
+ > ^ <
+"""
+
+# 创建窗口
+root = tk.Tk()
+root.title("Cute Cat 🐱")
+
+# 创建文本标签显示猫
+label = tk.Label(root, text=cat, font=("Consolas", 20), justify="center")
+label.pack(padx=20, pady=20)
+
+# 进入事件循环
+root.mainloop()
